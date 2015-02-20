f980e3ea5f09c2c75fe6eef06dd82804df6d09ff
Zachary J Felix <zf2153@messiah.edu>
2015-02-19 23:27:38 -0500

    Added script to run test inputs with known answers

---
+++ judging-compare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+class=${1%.*}
+inputs=$2
+javac "$class.java"
+
+if [ $? -ne 0 ] ; then
+    exit 1
+fi
+
+for file in judging/$inputs-*.in ; do
+    root=${file%.in}
+    if [ -e "$file" -a -e "$root.ans" ] ; then
+        echo "<<< $file >>>"
+        time java -client -Xss8m -Xmx2048m "$class" < "$file" | diff - "$root.ans"
+        if [ $? -ne 0 ] ; then
+            exit 1
+        fi
+    fi
+done
