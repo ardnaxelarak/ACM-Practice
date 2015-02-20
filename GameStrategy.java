89e42d727c13e9c3502922e66974ef5d156bbf80
Zachary J Felix <zf2153@messiah.edu>
2015-02-19 23:25:41 -0500

    GameStrategy: added support for multiple test cases in accordance with uva problem statement

--- GameStrategy.java
+++ GameStrategy.java
@@ -22,100 +22,105 @@ public class GameStrategy
 
     public static void main(String[] args)
     {
+        final int MAX_POS = 25;
+
         Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int m, k;
+        int n, m, k;
         boolean good;
+        int[][][] options;
+        LinkedList<int[]> curset = new LinkedList<int[]>();
+        int[] curopt;
+        int[][] path = new int[MAX_POS][MAX_POS];
 
-        int[][][] options = new int[n][][];
-        int[][] curset;
-        int count;
-        int[][] path = new int[n][n];
-        for (int i = 0; i < n; i++)
+        while (sc.hasNextInt())
         {
-            /* initialize solution array */
-            Arrays.fill(path[i], Integer.MAX_VALUE);
-            path[i][i] = 0;
+            n = sc.nextInt();
 
-            /* read input data */
-            m = sc.nextInt();
-            curset = new int[m][];
-            count = 0;
-            for (int j = 0; j < m; j++)
-            {
-                curset[j] = getOptions(sc.next(), i);
-                if (curset[j] != null)
-                    count++;
-            }
-            options[i] = new int[count][];
-            k = 0;
-            for (int j = 0; j < m; j++)
-            {
-                if (curset[j] != null)
-                    options[i][k++] = curset[j];
-            }
-        }
-
-        /* searching for start/end goals Alice can force in num moves */
-        for (int num = 1; num <= 25; num++)
-        {
+            options = new int[n][][];
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                /* initialize solution array */
+                Arrays.fill(path[i], Integer.MAX_VALUE);
+                path[i][i] = 0;
+
+                /* read input data */
+                m = sc.nextInt();
+                curset.clear();
+                for (int j = 0; j < m; j++)
                 {
-                    /* ignore goals for which we've already 
-                     * found a shorter path */
-                    if (path[i][j] < Integer.MAX_VALUE)
+                    curopt = getOptions(sc.next(), i);
+                    if (curopt == null)
                         continue;
 
-                    for (int[] optset : options[i])
-                    {
-                        good = true;
+                    if (curopt.length == 1)
+                        path[i][curopt[0]] = 1;
 
-                        /* look for an option set in which every choice
-                         * Bob makes leads to a win for Alice in num - 1
-                         * moves */
-                        for (int opt : optset)
+                    curset.add(curopt);
+                }
+                options[i] = new int[curset.size()][];
+                options[i] = curset.toArray(options[i]);
+            }
+
+            /* searching for start/end goals Alice can force in num moves */
+            for (int num = 2; num < n; num++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        /* ignore goals for which we've already 
+                         * found a shorter path */
+                        if (path[i][j] < Integer.MAX_VALUE)
+                            continue;
+
+                        for (int[] optset : options[i])
                         {
-                            if (path[opt][j] > num - 1)
+                            good = true;
+
+                            /* look for an option set in which every choice
+                             * Bob makes leads to a win for Alice in num - 1
+                             * moves */
+                            for (int opt : optset)
                             {
-                                /* Bob has an option that will take longer
-                                 * than num - 1 additional moves for Alice
-                                 * to win */
-                                good = false;
+                                if (path[opt][j] > num - 1)
+                                {
+                                    /* Bob has an option that will take longer
+                                     * than num - 1 additional moves for Alice
+                                     * to win */
+                                    good = false;
+                                    break;
+                                }
+                            }
+
+                            if (good)
+                            {
+                                /* Every option Bob takes leads to a win for
+                                 * Alice in num - 1 additional moves, or num
+                                 * total */
+                                path[i][j] = num;
                                 break;
                             }
-                        }
-
-                        if (good)
-                        {
-                            /* Every option Bob takes leads to a win for
-                             * Alice in num - 1 additional moves, or num
-                             * total */
-                            path[i][j] = num;
-                            break;
                         }
                     }
                 }
             }
-        }
-        
-        /* replace out default MAX_VALUE for not found with -1 */
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                if (path[i][j] == Integer.MAX_VALUE)
-                    path[i][j] = -1;
+            
+            /* replace out default MAX_VALUE for not found with -1 */
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    if (path[i][j] == Integer.MAX_VALUE)
+                        path[i][j] = -1;
 
-        /* print out solution */
-        for (int i = 0; i < n; i++)
-        {
-            System.out.print(path[i][0]);
-            for (int j = 1; j < n; j++)
+            /* print out solution */
+            for (int i = 0; i < n; i++)
             {
-                System.out.printf(" %d", path[i][j]);
+                System.out.print(path[i][0]);
+                for (int j = 1; j < n; j++)
+                {
+                    System.out.printf(" %d", path[i][j]);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
 }
