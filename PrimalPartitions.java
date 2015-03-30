9d9e7f2cac186317811fcb90e1fedbb36b9bcd39
Zachary J Felix <zf2153@messiah.edu>
2015-03-30 15:25:51 -0400

    PrimalPartitions: made some changes to improve efficiency

--- PrimalPartitions.java
+++ PrimalPartitions.java
@@ -3,6 +3,12 @@ import static java.lang.Math.*;
 
 public class PrimalPartitions
 {
+	static final int MAX_NUM = 1000000;
+	static int[][] D, F;
+	static int n, k;
+	static int[] hp;
+	static int[] list;
+
 	public static int gcd(int a, int b)
 	{
 		if (b > a)
@@ -17,86 +23,130 @@ public class PrimalPartitions
 		return b;
 	}
 
-    public static void main(String[] args)
-    {
-		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt();
-		int k = sc.nextInt();
-
-		final int MAX_NUM = 1000000;
-		int[] hp = new int[MAX_NUM + 1];
-		int cur, factor;
-		ListIterator<Integer> li;
+	public static void calcPrimes()
+	{
+		hp = new int[MAX_NUM + 1];
+		int cur;
 
 		hp[1] = 0;
 
-        for (int i = 2; i <= MAX_NUM; i++)
-            hp[i] = i;
+		for (int i = 2; i <= MAX_NUM; i++)
+			hp[i] = i;
 
-        for (int i = 2; i <= MAX_NUM; i++)
-        {
-            if (hp[i] == i)
-            {
-                for (int j = 2 * i; j <= MAX_NUM; j += i)
-                {
-                    cur = hp[j];
-                    while (cur > i && cur % i == 0)
-                        cur /= i;
-                    hp[j] = cur;
-                }
-            }
-        }
+		for (int i = 2; i <= MAX_NUM; i++)
+		{
+			if (hp[i] == i)
+			{
+				for (int j = 2 * i; j <= MAX_NUM; j += i)
+				{
+					cur = hp[j];
+					while (cur > i && cur % i == 0)
+						cur /= i;
+					hp[j] = cur;
+				}
+			}
+		}
+	}
 
-		int[][] D = new int[n][]; // gcd(l_j, ..., l_i)
-		int[][] F = new int[k + 1][n];
+	public static int getGCD(int start, int end)
+	{
+		int res;
+		assert end >= start;
+		if (start < 0)
+			return 1;
 
-		int[] list = new int[n];
+		if (D[end][start] != -1)
+			return D[end][start];
+
+		res = gcd(list[start], getGCD(start + 1, end));
+		D[end][start] = res;
+
+		// System.err.printf("gcd(%3d, %3d) = %d\n", start, end, res);
+
+		return res;
+	}
+
+	public static int getScore(int part, int len)
+	{
+		// System.err.printf("computing F(%d, %d)\n", part, len);
+		assert part > 0;
+		assert len > 0;
+
+		if (part > len)
+			part = len;
+
+		if (F[part][len] != -1)
+			return F[part][len];
+
+		int cur, next;
+		int score;
+		int best = 0;
+
+		next = list[len - 1];
+
+		for (int i = len - 1; i >= 0; i--)
+		{
+			// System.err.printf("i = %d\n", i);
+			cur = next;
+			// System.err.printf("cur = %d\n", cur);
+			if (cur == 1)
+				break;
+
+			next = getGCD(i - 1, len - 1);
+			// System.err.printf("next = %d\n", next);
+			if (hp[cur] == hp[next])
+				continue;
+
+			score = min(hp[cur], getScore(part - 1, i));
+			best = max(best, score);
+		}
+
+		F[part][len] = best;
+
+		// System.err.printf("  F(%3d, %3d) = %d\n", part, len, best);
+
+		return best;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+
+		n = sc.nextInt();
+		k = sc.nextInt();
+
+		calcPrimes();
+
+		D = new int[n][]; // gcd(l_j, ..., l_i)
+		F = new int[k + 1][n + 1];
+
+		for (int i = 2; i <= k; i++)
+			Arrays.fill(F[i], -1);
+
+		list = new int[n];
 		for (int i = 0; i < n; i++)
 			list[i] = sc.nextInt();
 
 		D[0] = new int[] {list[0]};
+		F[1][1] = hp[list[0]];
 
 		for (int i = 1; i < n; i++)
 		{
 			D[i] = new int[i + 1];
 			D[i][i] = list[i];
-			D[i][i - 1] = gcd(list[i], list[i - 1]);
-			for (int j = i - 2; j >= 0; j--)
-			{
-				D[i][j] = gcd(D[i][j + 1], D[i - 1][j]);
-			}
+			Arrays.fill(D[i], 0, i, -1);
 		}
 
+		int cur, last;
+		cur = list[0];
 		for (int i = 1; i < n; i++)
 		{
-			for (int j = 0; j <= i; j++)
-			{
-				D[i][j] = hp[D[i][j]];
-			}
-			// System.err.printf("D[%3d]: %s\n", i, Arrays.toString(D[i]));
+			last = cur;
+			cur = gcd(list[i], last);
+			D[i][0] = cur;
+			F[1][i + 1] = hp[cur];
 		}
 
-		for (int i = 0; i < n; i++)
-			F[1][i] = D[i][0];
-
-		// System.err.printf("F[%3d]: %s\n", 1, Arrays.toString(F[1]));
-		int max;
-
-		for (int i = 2; i <= k; i++)
-		{
-			for (int j = i - 1; j < n; j++)
-			{
-				max = 0;
-				for (int l = i - 1; l < j; l++)
-				{
-					max = max(max, min(F[i - 1][l], D[j][l + 1]));
-				}
-				F[i][j] = max;
-			}
-			// System.err.printf("F[%3d]: %s\n", i, Arrays.toString(F[i]));
-		}
-
-		System.out.println(F[k][n - 1]);
-    }
+		System.out.println(getScore(k, n));
+	}
 }
